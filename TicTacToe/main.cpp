700aba68368f4c19e49658c0221443d5c0c16280
emrepun <emrehavan@hotmail.com>
2019-01-21 19:45:58 +0000

    Game Completed

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -5,8 +5,22 @@ using namespace std;
 
 int main() {
 
+    char input;
+    bool isCompleted = false;
+
     TicTacToeGame game;
-    game.playGame();
+
+    while (!isCompleted) {
+        game.playGame();
+        cout << "Would you like to play again? (Y/N)";
+        cin >> input;
+
+        if (input == 'N' || input == 'n') {
+            isCompleted = true;
+        }
+    }
+
+
 
     return 0;
 }
