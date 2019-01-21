18399042ff9f24a4c6f40cb0fc53997decf4decd
emrepun <30630955+emrepun@users.noreply.github.com>
2019-01-21 21:14:31 +0000

    Refactored

--- TicTacToe/TicTacToeGame.h
+++ TicTacToe/TicTacToeGame.h
@@ -5,16 +5,11 @@
 #ifndef TICTACTOE_TICTACTOEGAME_H
 #define TICTACTOE_TICTACTOEGAME_H
 
-
 class TicTacToeGame {
 public:
     TicTacToeGame();
-
     void playGame();
-
-
 private:
-
     int getXCoord();
     int getYCoord();
     bool isValid(int input);
@@ -22,13 +17,10 @@ private:
     bool placeMarker(int x, int y, char currentPlayer);
 
     bool checkForVictory(char currentPlayer);
-
-
+    
     void clearBoard();
     void printBoard();
     char board[3][3];
-
 };
-
 
 #endif //TICTACTOE_TICTACTOEGAME_H
